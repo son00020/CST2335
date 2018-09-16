5b36ed6c4d4c8fc49bfd02eec8288d6062e81f93
son00020 <32390723+son00020@users.noreply.github.com>
2018-09-15 20:16:15 -0400

    new changes

---
+++ app/src/main/java/com/algonquinlive/son00020/androidlabs/StartActivity.kt
@@ -0,0 +1,12 @@
+package com.algonquinlive.son00020.androidlabs
+
+import android.app.Activity
+import android.os.Bundle
+
+class StartActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
